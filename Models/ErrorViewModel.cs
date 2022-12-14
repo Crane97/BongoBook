a73b28a2235c71f4ee340366c0902358d318daea
Jorge Ruiz <jorge@macdiego.com>
2022-12-14 08:44:28 +0100

    first

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BongoBookWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
